dea4866cb4b43692e9f1d729531ff7ec0632ce2a
Joel C. Osorio <joelosorio2006@gmail.com>
2025-09-21 09:43:29 -0600

    Funcion para actualizar la lista de amigos

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []; //Array que almacenara los nombre de amigos ingresados
 
 function agregarAmigo() {
     //Capturar el valor del campo de entrada
@@ -13,10 +14,19 @@ function agregarAmigo() {
     }
     //Se reestablece el campo de texto a una cadena vacía.
     input.value = "";
+    actualizarListaAmigos()
     return;
 }
 
-
-
+function actualizarListaAmigos() {
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    for (let i = 0; i < amigos.length; i++) {
+        let item = document.createElement("li");
+        item.textContent = amigos[i];
+        lista.appendChild(item);
+    }
+    return;
+}
 
 
