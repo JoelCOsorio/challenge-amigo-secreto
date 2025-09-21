4174c6778351372f781604f8024884e71db9ccc6
Joel C. Osorio <joelosorio2006@gmail.com>
2025-09-21 10:27:59 -0600

    Funcion para sortear amigos

--- app.js
+++ app.js
@@ -19,12 +19,23 @@ function agregarAmigo() {
 }
 
 function actualizarListaAmigos() {
-    let lista = document.getElementById("listaAmigos");
-    lista.innerHTML = "";
+    let lista = document.getElementById("listaAmigos");  //Obtener el elemento lista
+    lista.innerHTML = "";     //Limpiar la lista existente
+    //Crear elementos de lista (<li>) para cada nombre de amigo
     for (let i = 0; i < amigos.length; i++) {
-        let item = document.createElement("li");
-        item.textContent = amigos[i];
-        lista.appendChild(item);
+        let item = document.createElement("li");  //Crear elemento de lista
+        item.textContent = amigos[i];  //Asignar el nombre al elemento (<li>)
+        lista.appendChild(item);    //Agregar el elemento (<li>) a la lista
+    }
+    return;
+}
+
+function sortearAmigo() {
+    if (amigos.length > 0) {  //Verificar que el array amigos no este vacio
+        let indiceAlea = Math.floor(Math.random() * amigos.length); //Generar un indice aleatorio
+        let amigoEscogido = amigos[indiceAlea];  //Obtener el nombre sorteado
+        let lista = document.getElementById("resultado");
+        lista.innerHTML = `El amigo secreto sorteado es: ${amigoEscogido}`; //Mostrar el nombre sorteado
     }
     return;
 }
