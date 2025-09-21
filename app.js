df69414d33945c3695e0f304a7c6706214cc3d27
Joel C. Osorio <joelosorio2006@gmail.com>
2025-09-21 08:00:52 -0600

    Funcion que agrega el nombre de un amigo a un array

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
+function agregarAmigo() {
+    //Capturar el valor del campo de entrada
+    let input = document.getElementById("amigo");
+    let nombreAmigo = input.value;
+    //Se valida que el campo no esté vacío
+    if (nombreAmigo === "") {
+        alert("Por favor, inserte un nombre");
+    } else {
+        //Si el valor es válido, se añade al arreglo que almacena los nombre de amigos
+        amigos.push(nombreAmigo);
+    }
+    //Se reestablece el campo de texto a una cadena vacía.
+    input.value = "";
+    return;
+}
+
 
 
 
